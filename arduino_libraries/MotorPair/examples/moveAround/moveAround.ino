af37cd9c8dc1f2a230c5aaf1a44c05c7eb76636b
Rodrigo Caus <digocaus@hotmail.com>
2018-09-18 21:05:50 +0000

    O exemplo agora movimenta o robo em mais direções

--- arduino_libraries/MotorPair/examples/moveAround/moveAround.ino
+++ arduino_libraries/MotorPair/examples/moveAround/moveAround.ino
@@ -6,21 +6,39 @@ const int in2_pin = 6;
 const int in3_pin = 9;
 const int in4_pin = 10;
 
-MotorPair motores(in1_pin, in2_pin, in3_pin, in4_pin);
+MotorPair motors(in1_pin, in2_pin, in3_pin, in4_pin);
 
 void setup() {
 	delay(100);
 }
 
 void loop() {
-	for(int i=0; i<=250; i+=50) {
-		motores.setSpeed(i,-i);
-		delay(1000);
+	
+	for(int i=0; i<=250; i++) {
+		motors.setSpeed(i,-i);
+		delay(20);
 	}
-	for(int i=0; i<=250; i+=50) {
-		motores.setSpeed(-i,i);
-		delay(1000);
+	for(int i=0; i<=250; i++) {
+		motors.setSpeed(-i,i);
+		delay(20);
 	}
-	motores.stopMotors();
-	delay(3000);
+	motors.stopMotors();
+	delay(2000);
+
+	motors.setSpeed(100, 100);
+	delay(2000);
+	motors.setSpeed(100, 0);
+	delay(2000);
+	motors.setSpeed(0, 100);
+	delay(2000);
+	motors.stopMotors();
+	delay(500);
+	motors.setSpeed(-100, -100);
+	delay(2000);
+	motors.setSpeed(0, -100);
+	delay(2000);
+	motors.setSpeed(-100, 0);
+	delay(2000);
+	motors.stopMotors();
+	delay(2000);
 }
