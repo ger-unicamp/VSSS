be176e39159ada4734bddc74bdca73bc8bcbfb64
Rodrigo Caus <digocaus@hotmail.com>
2018-10-02 21:05:41 +0000

    "Mensagem agora tem uma função de depuração que retorna uma String. Os robôs começam do zero agora."

--- arduino_libraries/message/message.h
+++ arduino_libraries/message/message.h
@@ -2,16 +2,19 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
+#include <Arduino.h>
+
 typedef struct Message_st {
+  int left_speed0;
+  int right_speed0;
   int left_speed1;
   int right_speed1;
   int left_speed2;
   int right_speed2;
-  int left_speed3;
-  int right_speed3;
   int checksum;
 } Message;
 
-unsigned int hashString(Message msg);
+unsigned int hashMessage(Message msg);
+String messageToString(Message msg);
 
 #endif
