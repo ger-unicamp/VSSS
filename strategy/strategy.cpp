d5b85087cf209b91522f1fdca0f132eb30191556
Arthur Dadalto <arthurpd10@gmail.com>
2018-11-06 23:59:36 -0200

    Adding to_target function

--- strategy/strategy.cpp
+++ strategy/strategy.cpp
@@ -1,4 +1,5 @@
 #include "strategy.hpp"
+#include <armadillo>
 
 Strategy::Strategy(VSSSBuffer<GameState> &game_buffer, int *waitkey_buf, const char *serial_port, int baud)
 {
@@ -24,6 +25,8 @@ Strategy::~Strategy()
 	delete this->serial;
 }
 
+arma::vec2 to_target(Robot robot, arma::vec2 target);
+
 void Strategy::robot_control(VSSSBuffer<GameState> *game_buffer, int *waitkey_buf)
 {
 	vector<arma::vec2> robot_speed(3);
@@ -34,19 +37,7 @@ void Strategy::robot_control(VSSSBuffer<GameState> *game_buffer, int *waitkey_bu
 
 		cout << this->state.robots[0].pos << endl;
 
-		arma::vec2 target = state.ball.pos;
-
-		arma::vec2 dir_target = arma::normalise(target - this->state.robots[0].pos);
-
-		arma::vec3 c_prod = arma::cross(arma::vec3({this->state.robots[0].dir[0], this->state.robots[0].dir[1], 0.0}), arma::vec3({dir_target[0], dir_target[1], 0.0}));
-		double c_prod_sign = c_prod[2] == 0 ? 1 : (c_prod[2] / abs(c_prod[2]));
-
-		double theta = acos(arma::dot(dir_target, this->state.robots[0].dir));
-
-		double diff = max(40.0, theta * 10);
-		double fwd = max(100.0, min(40.0, sqrt(arma::norm(target - this->state.robots[0].pos, 2))));
-
-		robot_speed[0] = {fwd - c_prod_sign * diff, fwd + c_prod_sign * diff};
+		robot_speed[0] = to_target(this->state.robots[0], state.ball.pos);
 
 		/*
 		if (this->state.robots[0].pos[0] < FIELD_WIDTH / 2.0)
@@ -71,4 +62,19 @@ void Strategy::robot_control(VSSSBuffer<GameState> *game_buffer, int *waitkey_bu
 			printf("Speed: %lf %lf %lf %lf %lf %lf\n", robot_speed[0][0], robot_speed[0][1], robot_speed[1][0], robot_speed[1][1], robot_speed[2][0], robot_speed[2][1]);
 		}
 	}
+}
+
+arma::vec2 to_target(Robot robot, arma::vec2 target)
+{
+	arma::vec2 dir_target = arma::normalise(target - robot.pos);
+
+	arma::vec3 c_prod = arma::cross(arma::vec3({robot.dir[0], robot.dir[1], 0.0}), arma::vec3({dir_target[0], dir_target[1], 0.0}));
+	double c_prod_sign = c_prod[2] == 0 ? 1 : (c_prod[2] / abs(c_prod[2]));
+
+	double theta = acos(arma::dot(dir_target, robot.dir));
+
+	double diff = max(40.0, theta * 10); // TODO find the best parameters for both these numbers
+	double fwd = max(100.0, min(40.0, sqrt(arma::norm(target - robot.pos, 2))));
+
+	return arma::vec2({fwd - c_prod_sign * diff, fwd + c_prod_sign * diff}); // TODO maybe use {fwd, fwd + diff}
 }
